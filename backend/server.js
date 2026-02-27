af0cf86cade2a9611bd7b64f497018830e3a694c
aadarshantony <adarshantony223@gmail.com>
2026-02-27 21:31:57 +0530

    Basic Structure

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,25 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import connectDB from "./config/db.js";
+
 dotenv.config();
+connectDB();
 
 const PORT = process.env.PORT
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONT_END_URL,
+    credentials: true
+}));
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("Api is up and running!");
 })
+
 
 app.listen(PORT, () => {
     console.log(`Listening to ${PORT}`);
